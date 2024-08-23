4128353a287f9060d8efc4edcf47a1b14901e0a1
Andrew Faulkner <a.g.flknr@gmail.com>
2024-08-23 19:13:15 -0400

    Clean up repo
     - Remove unused files
     - Add GitHub link

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Assets from "./assets";
 import philosophers from "./data/philosophers.json";
 import NivoTimeline from "./NivoTimeline";
 
@@ -9,11 +10,32 @@ export interface Philosopher {
 }
 
 function App() {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    // dark mode
+  }
+
+  const gitHubLogo =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? Assets.GitHubLogoWhite
+      : Assets.GitHubLogoBlack;
+
   return (
     <>
       <h1>Philosophers Timeline</h1>
 
       <NivoTimeline philosophers={philosophers} />
+
+      <a
+        href="https://github.com/AndyF5/philosophers-timeline"
+        id="github-link"
+      >
+        <img src={gitHubLogo} id="github-logo" />
+        GitHub
+      </a>
     </>
   );
 }
---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import GitHubLogoBlack from "./github-mark.png";
+import GitHubLogoWhite from "./github-mark-white.png";
+
+const Assets = {
+  GitHubLogoBlack,
+  GitHubLogoWhite,
+};
+
+export default Assets;
