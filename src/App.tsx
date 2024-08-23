e22478872ad71ca178993ba2223729e8156a0f10
Andrew Faulkner <a.g.flknr@gmail.com>
2024-08-23 16:20:14 -0400

    feat: Functional timeline using Nivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import philosophers from "./data/philosophers.json";
+import NivoTimeline from "./NivoTimeline";
+
+export interface Philosopher {
+  Philosopher: string;
+  Born: string;
+  Died: string;
+}
 
 function App() {
   return (
     <>
       <h1>Philosophers Timeline</h1>
+
+      <NivoTimeline philosophers={philosophers} />
     </>
   );
 }
